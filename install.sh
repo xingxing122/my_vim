faff517ee2f90d3a283eed2b99b0e7e1815a9e33
xingxing <xingxingwang0122@gmail.com>
2016-11-09 15:56:57 +0800

    安装说明

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+# xingxing 
+#QQ群　150181442
+#rhel/centos 
+yum install git  
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+#下载.vimrc 
+wget https://raw.githubusercontent.com/xingxing9688/my_vim/master/.vimrc 
+
