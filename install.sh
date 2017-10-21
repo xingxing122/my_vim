bcba1a5026d2f743c264da46d056df281741e98f
xingxingwang <xingxingwang122@gmail.com>
2017-10-21 10:01:49 +0800

    book

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@
 yum install git  
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 #下载.vimrc 
-wget https://raw.githubusercontent.com/xingxing9688/my_vim/master/.vimrc 
+    wget https://raw.githubusercontent.com/xingxing9688/my_vim/master/.vimrc 
 
 
 
